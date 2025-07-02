6b2e9ba2a229f13aa06bace486aceab0a9d9ba28
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-02 17:18:44 -0500

    created a quick setup welcome page before game starts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
+  const [gameStart, setGameStart] = useState(false);
+
+  // Display welcome page before game has started
+  if (!gameStart) {
+    return (
+      <>
+        <h1>Whack-A-Mole</h1>
+        <button onClick={() => setGameStart(true)}>Start Game</button>
+      </>
+    );
+  }
+  // Display game page after clicking start
   return <></>;
 }
