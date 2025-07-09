d699bf518d44403b30ab62ec1a0bf7a28fcd37f6
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-09 12:28:41 -0500

    styled buttons on the game options to be more visable and user friendly

--- src/game/GameOptions.jsx
+++ src/game/GameOptions.jsx
@@ -4,22 +4,36 @@ import { GameContext } from "./GameContext";
 export default function GameOptions() {
   const game = useContext(GameContext);
 
-  if (game.gameStatus === "ongoing") {
+  function setOptions() {
+    if (game.gameStatus === "ongoing") {
+      return (
+        <>
+          <div className="gameButtons">
+            <button onClick={() => game.stopGame()}>stop</button>
+            <button onClick={() => game.restartGame()}>restart</button>
+          </div>
+        </>
+      );
+    }
+
     return (
-      <div className="gameOptions">
-        <button onClick={() => game.stopGame()}>stop</button>
-        <div>score: {game.gameScore}</div>
-        <button onClick={() => game.restartGame()}>restart</button>
-      </div>
+      <>
+        <button onClick={() => game.startGame()}>start</button>
+        <div>
+          <p>Board Size: </p>
+          <button onClick={() => game.decreaseGameSize()}>decrease </button>
+          <button onClick={() => game.increaseGameSize()}>increase</button>
+        </div>
+      </>
     );
   }
 
   return (
     <div className="gameOptions">
-      <button onClick={() => game.startGame()}>start</button>
-      <div>score: {game.gameScore}</div>
-      <button onClick={() => game.decreaseGameSize()}>decrease </button>
-      <button onClick={() => game.increaseGameSize()}>increase</button>
+      <div className="gameScore">
+        <p>score: {game.gameScore}</p>
+      </div>
+      <div className="gameButtons">{setOptions()}</div>
     </div>
   );
 }
