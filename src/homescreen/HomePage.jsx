af6ec128863ac866b691f7e9e628e8d419e9ae6e
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-09 18:51:17 -0500

    added game instructions to homepage

--- src/homescreen/HomePage.jsx
+++ src/homescreen/HomePage.jsx
@@ -4,6 +4,13 @@ export default function HomePage({ setGameStart }) {
   return (
     <div className="homepage">
       <h1>Whack-A-Mole</h1>
+      <p>
+        Hit the mole and win a prize. The prize of a bigger score.
+        <br />
+        When the mole is hit your score will increase by 1 and the mole will change locations. Go as
+        far as you can. <br />
+        Good luck!
+      </p>
       <button onClick={() => setGameStart(true)}>Start Game</button>
     </div>
   );
